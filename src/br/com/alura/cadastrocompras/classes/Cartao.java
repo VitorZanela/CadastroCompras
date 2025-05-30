da6df27781b91d316441419ad5b1b8c982194c6f
vitorzanela <vitor.mardig@gmail.com>
2025-05-29 23:16:03 -0300

    Todas inclusões de Atributos e metodos finalizados

--- src/br/com/alura/cadastrocompras/classes/Cartao.java
+++ src/br/com/alura/cadastrocompras/classes/Cartao.java
@@ -1,4 +1,13 @@
 package br.com.alura.cadastrocompras.classes;
 
 public class Cartao {
+    private double limite;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
 }
